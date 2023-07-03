d77d1bb0d19b8b249ae1c5afad8efcac26234db8
cossack910 <maris160zhk@gmail.com>
2023-07-03 21:09:27 +0900

    風邪ひいた

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,14 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Department } from "./department"; // exportを付けたクラスを呼び出す
-
-const acc = new Department("経理", [], "aa");
-acc.decribe();
-acc.addEmployee("ドラエモン");
-console.log(acc.employees);
-
-// const keiriCopy = { name: "経理2", describe: keiri.decribe };
-// keiriCopy.describe();
 
 function App() {
   return (
