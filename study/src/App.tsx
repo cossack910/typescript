2f3d075c103e17d50ce1a889fa9067a9f5c25ce3
cossack910 <maris160zhk@gmail.com>
2023-07-11 21:51:24 +0900

    デコレータ追加

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./App.css";
+import { Product } from "./decorator"; // exportを付けたクラスを呼び出す
 
 function App() {
+  new Product("a", 100);
   return (
     <div className="App">
       <header className="App-header"></header>
--- study/src/decorator.tsx
+++ study/src/decorator.tsx
@@ -1,18 +1,58 @@
-function WithTemplate(template: string, hookId: string) {
-  return function (_: Function) {
-    const hookEl = document.getElementById(hookId)!;
-    if (hookEl) {
-      hookEl.innerHTML = template;
-    }
-  };
+//プロパティデコレーター
+function Log(target: any, propatyName: string | Symbol) {
+  console.log("プロパティデコレータ");
+  console.log(target, propatyName);
 }
 
-//@Logger("引数を渡す")
-@WithTemplate("<h1>やったぜ</h1>", "App")
-class Person {
-  name = "Max";
+//アクセサデーコレータ
+function Log2(target: any, name: string, decriptor: PropertyDescriptor) {
+  console.log("Accessor デコレーター");
+  console.log(target);
+  console.log(name);
+  console.log(decriptor);
+}
 
-  constructor() {
-    console.log("Personオブジェクト作成中...");
+//メソッドデコレータ
+function Log3(
+  target: any,
+  name: string | Symbol,
+  decriptor: PropertyDescriptor
+) {
+  console.log("メソッド デコレーター");
+  console.log(target);
+  console.log(name);
+  console.log(decriptor);
+}
+
+//パラメータデコレーター
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("パラメータ デコレーター");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+export class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("不正価格");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
   }
 }
